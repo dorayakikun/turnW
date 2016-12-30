037b93de201d519770d60713349657ac77d07822
dorayaki-kun <dorayaki.tomo@gmail.com>
2016-12-30 19:11:22 +0900

    :sparkles: Enable to hear hiragana and katakana

--- server.js
+++ server.js
@@ -13,11 +13,9 @@ controller.spawn({
     token: process.env.NODE_SLACK_BOT_TOKEN,
 }).startRTM();
 
-controller.hears('(\\\\[\w\\\.]*|"\\\\[\w\.].*")', function(bot, message) {
-    var matches = message.text.match(/(\\\\[\w\\\.]*|"\\\\[\w\.].*")/);
+controller.hears(['(\\\\[\w\\\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF]*|"\\\\[\w\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF].*")'], ['ambient', 'mention'], function(bot, message) {
+    var matches = message.text.match(/(\\\\[\w\\\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF]*|"\\\\[\w\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF].*")/);
     bot.reply(message, `
 file:${matches[0].replace(/\\/g, '/')}
-smb:${matches[0].replace(/\\/g, '/')}
-`);
+smb:${matches[0].replace(/\\/g, '/')}`);
 });
-

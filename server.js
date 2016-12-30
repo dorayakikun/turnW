0178aac61ee34b197ea5438c46a257cf8f474beb
dorayaki-kun <dorayaki.tomo@gmail.com>
2016-12-30 22:03:12 +0900

    :sparkles: Enable to reaction to the message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var controller = Botkit.slackbot({
 });
 
 if (!process.env.NODE_SLACK_BOT_TOKEN) {
-    throw new Error('Error: specify token in environment');
+    throw new Error('Failed to specify token in environment');
 }
 
 // connect the bot to a stream of messages
@@ -13,9 +13,24 @@ controller.spawn({
     token: process.env.NODE_SLACK_BOT_TOKEN,
 }).startRTM();
 
-controller.hears(['(\\\\[\w\\\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF]*|"\\\\[\w\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF].*")'], ['ambient', 'mention'], function(bot, message) {
+controller.hears(['(\\\\[\w\\\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF]*|"\\\\[\w\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF].*")'], ['ambient'], function(bot, message) {
     var matches = message.text.match(/(\\\\[\w\\\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF]*|"\\\\[\w\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF].*")/);
-    bot.reply(message, `
+
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'crab',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        bot.reply(message, `
 file:${matches[0].replace(/\\/g, '/')}
 smb:${matches[0].replace(/\\/g, '/')}`);
+    });
 });

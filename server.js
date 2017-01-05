e085f40abf996f9550a740f93b84038b17d5d376
dorayaki-kun <dorayaki.tomo@gmail.com>
2017-01-05 22:09:50 +0900

    :art: Refactor regular expression

--- server.js
+++ server.js
@@ -13,22 +13,20 @@ controller.spawn({
     token: process.env.NODE_SLACK_BOT_TOKEN,
 }).startRTM();
 
-controller.hears(['(\\\\[\w\\\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF]*|"\\\\[\w\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF].*")'], ['ambient'], function(bot, message) {
-    var matches = message.text.match(/(\\\\[\w\\\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF]*|"\\\\[\w\.\u3041-\u3096\u30A1-\u30FA\u3400-\u9FFF\uF900-\uFAFF].*")/);
-
+controller.hears(['(\\\\[\w\\\.\-_\\\/\$%~\u3040-\uFAFF]*|"\\\\[\w\.\-_\\\/\$%~\u3040-\uFAFF].*")'], ['ambient'], function (bot, message) {
+    var matches = message.text.match(/(\\\\[\w\\\.\-_\\\/\$%~\u3040-\uFAFF]*|"\\\\[\w\.\-_\\\/\$%~\u3040-\uFAFF].*")/);
 
     bot.api.reactions.add({
         timestamp: message.ts,
         channel: message.channel,
         name: 'crab',
-    }, function(err, res) {
+    }, function (err, res) {
         if (err) {
             bot.botkit.log('Failed to add emoji reaction :(', err);
         }
     });
 
-
-    controller.storage.users.get(message.user, function(err, user) {
+    controller.storage.users.get(message.user, function (err, user) {
         bot.reply(message, `
 file:${matches[0].replace(/\\/g, '/')}
 smb:${matches[0].replace(/\\/g, '/')}`);

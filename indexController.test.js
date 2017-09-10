da3fc5a59b547daaaada294dda30be596f865d02
dorayakikun <amakunai.dorayaki@gmail.com>
2017-09-10 13:52:09 +0900

    :green_heart: Add tests

---
+++ indexController.test.js
@@ -0,0 +1,38 @@
+const assert = require('assert');
+const Botmock = require('botkit-mock');
+const myController = require("./indexController");
+
+describe("controller tests",()=>{
+  beforeEach(()=>{
+    this.controller = Botmock({});
+    // type can be ‘slack’, facebook’, or null
+    this.bot = this.controller.spawn({type: 'slack'});
+    myController(this.controller);
+  });
+
+  it('should return uri text`', () => {
+    return this.bot.usersInput(
+      [
+        {
+          user: 'someUserId',
+          channel: 'someChannel',
+          messages: [
+            {
+              text: '\\\\myFileServer\\tests', isAssertion: true
+            }
+          ]
+        }
+      ]
+    ).then((message) => {
+      // In message, we receive a full object that includes params:
+      // {
+      //    user: 'someUserId',
+      //    channel: 'someChannel',
+      //    text: 'help message',
+      // }
+      return assert.equal(message.text, `
+file://myFileServer/tests
+smb://myFileServer/tests`);
+    })
+  });
+});
